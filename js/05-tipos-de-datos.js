a8f7ef7a74c4cecc68b99edc6344f38b71b2b1e3
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-15 01:54:39 -0500

    Tipos de datos y operadores
    Vimos como hacer una operación con los diferentes tipos de datos.

--- js/05-tipos-de-datos.js
+++ js/05-tipos-de-datos.js
@@ -1,5 +1,27 @@
 'use strict'
 
 //Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+var operacionResta = numero1 - numero2;
+var operacionMulti = numero1 * numero2;
+var operacionDiv = numero1 / numero2;
+
+alert ("El resultado de la suma es: "+operacion);
+console.log(operacionResta);
+console.log(operacionMulti);
+console.log(operacionDiv);
 
 //Tipos de datos
+//Estero, String, boleano, array, objetos
+var numer_entero = 44;
+var cadena_texto = "Hola que tal";
+var verdadero_o_falso = true; // true (1) ó false(0)
+
+var numero_falso = "33";
+
+console.log(Number(numero_falso)+7.3);//para comvertirlo en numero
+console.log(parseInt(numero_falso)+7.3);//para comvertirlo en numero entero
+console.log(String(numer_entero)+7);//para comvertirlo a texto
+
