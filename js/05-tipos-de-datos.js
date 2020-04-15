7aae21774299416cd0e7fe398133141a7090d6bc
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-15 02:07:30 -0500

    TypeOf
    Aplicamos comentario a todas las salidas a pantalla y a consola para poder mandar nuevas sentancias a consola mostrando el tipo de dato que es la variable con un "typeof".

--- js/05-tipos-de-datos.js
+++ js/05-tipos-de-datos.js
@@ -8,10 +8,12 @@ var operacionResta = numero1 - numero2;
 var operacionMulti = numero1 * numero2;
 var operacionDiv = numero1 / numero2;
 
+/*
 alert ("El resultado de la suma es: "+operacion);
 console.log(operacionResta);
 console.log(operacionMulti);
 console.log(operacionDiv);
+*/
 
 //Tipos de datos
 //Estero, String, boleano, array, objetos
@@ -21,7 +23,13 @@ var verdadero_o_falso = true; // true (1) ó false(0)
 
 var numero_falso = "33";
 
+/*
 console.log(Number(numero_falso)+7.3);//para comvertirlo en numero
 console.log(parseInt(numero_falso)+7.3);//para comvertirlo en numero entero
 console.log(String(numer_entero)+7);//para comvertirlo a texto
+*/
 
+console.log (typeof numer_entero);
+console.log(typeof cadena_texto);
+console.log(typeof verdadero_o_falso);
+console.log(typeof numero_falso);
