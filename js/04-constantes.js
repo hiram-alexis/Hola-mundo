dbb2d9f3062d8eb83017389cbe76b290426304c1
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-15 00:54:15 -0500

    Constantes
    Ejemplo de como funciona una constante y lo que no se puede hacer.

---
+++ js/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//contantes
+// es como una variable pero su valor no puede ser modificado.
+
+var web ="httos//www.google.com";
+const ip = "192.88.0.12";
+
+web = "http://www.youtube.com";
+ip = "2232";
+
+console.log(web, ip);
