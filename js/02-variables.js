7c7807f3fb2d1bc71d7824c103d150b818a5e22e
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-14 23:58:34 -0500

    Variables
    Se definen variables dentro de el nuevo js.

--- js/02-variables.js
+++ js/02-variables.js
@@ -1,1 +1,14 @@
 //VARIABLES
+//Es un contenedor de infromación. Guardara cualquier tipo de dato.
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+
+
+pais= "México";
+continente = "Latinoamerica";
+var pais_y_continente = pais+ ' ' +continente
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
