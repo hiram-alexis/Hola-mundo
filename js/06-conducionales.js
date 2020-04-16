4acb35bad1d2f9154510eca0460bf243791cbe40
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-16 02:26:15 -0500

    condicionales y operadores logicos
    agregamos a nuestro conocimiento la parte de los operadores logicos: &&, ||, !=, etc. #1

--- js/06-conducionales.js
+++ js/06-conducionales.js
@@ -44,3 +44,28 @@ if (edad <= 33) {
 	//Es menor de edad
 console.log(nombre+" tiene "+edad+ " años, es MENOR de edad.");	
 }
+/*
+AND(Y): &&
+OR(O): ||
+NEGACION: !
+*/
+
+//Negación
+var year = 2038;
+if (year != 2016) {
+	console.log("El año no es 2016 realmente es: "+year);
+}
+
+//AND
+if (year >= 2000 && year<= 2020) {
+	console.log("Estamos en la era actual");
+} else {
+	console.log("Estamos en la era post moderna");
+}
+
+//OR
+if (year == 2008 || (year >= 2018 && year ==2028)) {
+	console.log("El año acaba en 8");
+} else {
+	console.log("Año no registrado");
+}
