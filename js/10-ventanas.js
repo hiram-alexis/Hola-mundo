8cd85fbe56bc7cf1374d43d42710ad5582dbd3c4
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-17 01:39:30 -0500

    Alertas y ventanas
    Los tipos de alertas y de ventanas que podemos utilizar dentro de nuestros programas.
    #3
    closes #3

---
+++ js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//ALERTA
+alert("Esta es mi alerta");
+
+
+//CONFIRMACIÓN
+var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+//INGRESO DE DATOS
+var mi_resultado = prompt("¿Que edad tienes?", 18);
+console.log(Number(mi_resultado));
