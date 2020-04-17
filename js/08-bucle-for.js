2a574c2249841bdd133a334e040492d98a900857
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-16 23:55:04 -0500

    Buble FOR
    Estructura y como funciona un ciclo FOR. #2

---
+++ js/08-bucle-for.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Bucle FOR
+// es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for (var i = 0; i <= numero; i++){
+	console.log("VAmos por el numero "+i);
+	debugger;
+}
