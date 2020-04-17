8165d0e28286f0dc0bec2c8c395ab40243c3c386
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-17 00:26:34 -0500

    buble while
    video 28. Buble While.
    aplicamos lo aprendido en un nuevo archivo .js y cambiamos el direccionamiento del index. #2

--- js/08-bucle-for.js
+++ js/08-bucle-for.js
@@ -7,5 +7,5 @@ var numero = 100;
 
 for (var i = 0; i <= numero; i++){
 	console.log("VAmos por el numero "+i);
-	debugger;
+	//debugger;
 }
---
+++ js/09-while.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//Bucle while
+//Parecido al ciclo for, pero menos estricto de forma 
+//que puedo hacer ciclos infinitos o indefinidos.
+
+var year = 2018;
+//Mientras que pase la condición
+while( year != 1991){
+//Se ejecuta esto
+console.log("Estamos en el año: "+ year);
+
+//year ++;
+year --;
+}
+
