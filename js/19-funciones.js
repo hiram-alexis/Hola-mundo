9e5d7335625ec57614bd5966972ee9cdefdd2bd2
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-21 03:17:17 -0500

    Parametros
    Como se ocupan los parametros.
    #5

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -3,12 +3,15 @@
 //Funciones: Son un grupo de ordenes agrupados por un nombre en concreto.
 //Una función es una agrupación reutilizable de un conjunto de instrucciones.
 
-//defino la cunción
+//defino la función
+/*	
 function calculadora(){
 	//Le agrego el conjunto de instrucciones a ejecutar.
-	console.log("Hola soy la calculadora");
-	console.log("Si, soy yo");
-	
+	//console.log("Hola soy la calculadora");
+	//console.log("Si, soy yo");
+
+ console.log("Suma: "+(10+15));
+
 	return "Hola soy la calculadora!";
 }
 //invocar o llamar a la función
@@ -16,3 +19,27 @@ function calculadora(){
 var resultado = calculadora();
 
 console.log(resultado);
+*/
+
+//PARAMETROS---------------------------------------------
+//Son variables que alteran la lógica o el resoltado de la función
+//abstraemos más de la lógica y hacemos más reutilizable una función.
+function calculadora(numero1, numero2){
+	//Le agrego el conjunto de instrucciones a ejecutar.
+		
+ console.log("Suma: "+(numero1+numero2));
+  console.log("Resta: "+(numero1-numero2));
+   console.log("Multiplicaión: "+(numero1*numero2));
+    console.log("División: "+(numero1/numero2));
+    console.log("********************************************");
+	//return "Hola soy la calculadora!";
+}
+//invocar o llamar a la función
+calculadora(12, 8);
+calculadora(98, 2);
+
+for (var i = 1 - 1; i <= 10; i++) {
+	console.log(i);
+	calculadora(i, 8);
+}
+
