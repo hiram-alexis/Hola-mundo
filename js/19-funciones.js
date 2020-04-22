3dab213b187c573df85a4248df5fe9a1fdbad019
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-22 02:23:53 -0500

    funciones opcionales
    reacomodamos el js para poder introducir opcionalmente otra función.
    #5

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -24,17 +24,31 @@ console.log(resultado);
 //PARAMETROS---------------------------------------------
 //Son variables que alteran la lógica o el resoltado de la función
 //abstraemos más de la lógica y hacemos más reutilizable una función.
-function calculadora(numero1, numero2){
+function calculadora(numero1, numero2, mostrar = false){
 	//Le agrego el conjunto de instrucciones a ejecutar.
-		
- console.log("Suma: "+(numero1+numero2));
-  console.log("Resta: "+(numero1-numero2));
-   console.log("Multiplicaión: "+(numero1*numero2));
-    console.log("División: "+(numero1/numero2));
-    console.log("********************************************");
+	if (mostrar == false) {
+			console.log("Suma: "+(numero1+numero2));
+ 			console.log("Resta: "+(numero1-numero2));
+   			console.log("Multiplicaión: "+(numero1*numero2));
+   			console.log("División: "+(numero1/numero2));
+    		console.log("********************************************");
+
+
+	}	else{
+		 	document.write("Suma: "+(numero1+numero2)+"<br/>");
+  			document.write("Resta: "+(numero1-numero2)+"<br/>");
+   			document.write("Multiplicaión: "+(numero1*numero2)+"<br/>");
+    		document.write("División: "+(numero1/numero2)+"<br/>");
+    		document.write("********************************************"+"<br/>");
+
+
+	}
 	//return "Hola soy la calculadora!";
 }
 //invocar o llamar a la función
+calculadora(1, 4);
+calculadora(2, 5, true);
+/*
 calculadora(12, 8);
 calculadora(98, 2);
 
@@ -42,4 +56,6 @@ for (var i = 1 - 1; i <= 10; i++) {
 	console.log(i);
 	calculadora(i, 8);
 }
+*/
+
 
