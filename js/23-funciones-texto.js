1e8591d93222a481b512695febe9cfa702cbbf97
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-24 13:26:06 -0500

    50.Metodos de busqueda
    diferente formas de realizar una busqueda de texto. #5

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -3,9 +3,9 @@
 //Transformacion de textos de variables
 
 var numero = 444;
-var texto1 = "Bienvenido al curso JavaScript";
+var texto1 = "Bienvenido al curso JavaScript, curso valioso";
 var texto2 = " Es un buen curso";
-
+/*
 var dato = numero.toString();
 	//dato = texto1.toUpperCase(); //Convertir a mayusculas.
 	dato = texto1.toLowerCase();	//Convertir a minusculas
@@ -25,3 +25,16 @@ var dato = numero.toString();
  
 var textoTotal = texto1.concat (""+texto2); //concatenar textos
  console.log(textoTotal);
+ */
+
+ //var busqueda = texto1.indexOf("curso");//La primera vez que aparece la palabra
+ //var busqueda = texto1.lastIndexOf("curso"); //Ultima vez que aparece la palabra
+ //var busqueda = texto1.search("curso");
+//var busqueda = texto1.match(/curso/g); //nos arroja un array con cuanto se repite.
+//var busqueda = texto1.substr(14,5);// para que la busqueda de a partir del caractec 14 y de el numero de caracteres que pida
+//var busqueda = texto1.charAt(22); //una letra en una posicion especifica
+//var busqueda = texto1.startsWith("Bienvenido");//nos va a decir si esta la palabra en el inicio.
+//var busqueda = texto1.endsWith("valioso");//nos va a decir si esta la palabra en el final.
+var busqueda = texto1.includes("JavaScript");//nos va a decir si esta la palabra en el final.
+
+ console.log(busqueda);	//otra forma igual a la primera de hacer busqueda de texto.
