9ad7168ccf82299ab03812d3306602b955c642db
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-04-24 03:16:23 -0500

    46.funciones anónimas y callbacks

---
+++ js/21-funciones-anonimas.js
@@ -0,0 +1,33 @@
+'use strict'
+
+//Funciones anonimas
+//Una funcion que no tiene nombre
+//Callbacks funcion dentro de otra funcion
+/*
+var pelicula = function (nombre){
+	return "La pelicula es: "+nombre;
+}
+*/
+
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
+	var sumar = numero1 + numero2;
+
+	sumaYmuestra(sumar);
+	sumaPorDos(sumar);
+
+	return sumar;
+}
+
+sumame(5,7, function(dato){
+	console.log("La funcion es: ", dato)
+},
+function(dato){
+	console.log("La suma por dos es: ", dato*2)
+}
+);
+/*
+console.log(
+	sumame(4,5)
+	);
+	*/
+
