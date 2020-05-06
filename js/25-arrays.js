11336e774f1e75cbf53b31238a89b3ae9ae65c94
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-06 02:36:23 -0500

    Arrays básicos
    Practica de video 53.Arrays básicos

---
+++ js/25-arrays.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Arrays, Arreglos, Matrices
+//Es una coleccion de datos que puede tener una variable.
+
+var nombre = "Alexo Sánchez"; //una variable normal
+var nombres = ["Alexis Sánchez", "Vero Sama", "Sefora Sánchez"]; // Variable con array de nombres
+			//		Posicion0		Posicion 1	Posicion 2
+var lenguajes = new Array ("PHP","JS","Go","Java"); //Otra forma de declarar un Array
+
+console.log(nombres[0]); //Mandamos a llamar el primer elemento 
