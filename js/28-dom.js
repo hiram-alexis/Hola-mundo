b6d70ce32f2f13072c4d68e50bce370b121b81f8
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-09 22:44:50 -0500

    Selleccionar y modificar elementos de la pagina.
    En este video tuve problemas ya que el que esta explicando hace corte al video y despues del corte ya vuelve con varios cambios que hay que estar cachando. #8

--- js/28-dom.js
+++ js/28-dom.js
@@ -6,17 +6,44 @@ function cambiaColor (color){
 	caja.style.background = color;
 }
 
-
+//Coseguir elementos con un ID concreto
 
 //var caja = document.getElementById("micaja");
 var caja = document.querySelector("#micaja"); 
 
 caja.innerHTML ="¡Texto en la caja desde js!";
+
 caja.style.background = "red";
 caja.style.padding = "20px";
 caja.style.color = "white";
 caja.className = "hola hola2"; 
 console.log(caja);
+
+//Conseguir elementos por su etiqueta
+var todosLosDivs = document.getElementsByTagName('div');
+//var contenidoEnTexto = todosLosDivs[2].textContent;
+//todosLosDivs.forEach((valor, indice)=>{
+var seccion = document.querySelector("#miseccion");
+
+var hr =document.createElement("hr");
+
+
+var valor;
+for(valor in todosLosDivs){
+	if(typeof todosLosDivs[valor].textContent == 'string'){
+		var parrafo = document.createElement("p");
+		var texto = document.createTextNode(todosLosDivs[valor].textContent);
+		parrafo.append(texto);
+		seccion.append(parrafo);
+	}
+	
+}
+seccion.append(hr);
+
+//})
+
+//console.log(contenidoEnTexto);
+//Conseguir elementos por su clase
 
 
 //black purple pinck blue red brown 
