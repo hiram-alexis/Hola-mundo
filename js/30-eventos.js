7a766f883766586126fe60ebe2d848958dbc640f
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-13 03:03:19 -0500

    70. Eventos de teclado y foco, etc
    Eventos que responden al teclear, al dejar de teclear, al estar dentro o fuera. #9

--- js/30-eventos.js
+++ js/30-eventos.js
@@ -32,3 +32,30 @@ boton.addEventListener('mouseover', function(){
 boton.addEventListener('mouseout', function(){
 	boton.style.background ="brown";
 });
+
+//Focus
+var input = document.querySelector("#campo_nombre"); 
+
+input.addEventListener('focus', function(){
+	console.log("[focus] Estas dentro del input");
+});
+
+//Blur
+input.addEventListener('blur', function(){
+	console.log("[blur] Estas fuera del input");
+});
+//Keydown
+input.addEventListener('keydown', function(event){
+	console.log("[keydown] Pulsando esta tecla", String.fromCharCode(event.keyCode));
+
+});
+//Keypress
+input.addEventListener('keypress', function(event){
+	console.log("[keypress] Tecla presionada", String.fromCharCode(event.keyCode));
+
+});
+//Keyup
+input.addEventListener('keyup', function(event){
+	console.log("[keypress] Tecla soltada", String.fromCharCode(event.keyCode));
+
+});
