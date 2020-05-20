7a5807650fc6295d00c7c4034c264fb8299fcedb
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-20 03:53:03 -0500

    83. Capturar errores
    Capturamos en que linea hay un error usando un Try & Catch. #13

---
+++ js/37-try-catch.js
@@ -0,0 +1,11 @@
+'use strict'
+try{
+var year = 2019;
+alert(year);
+var vector = new Array(9999999999);	//No se puede definir un array de esas medidas
+console.log();
+
+}catch(error){
+	console.log(error);
+	alert("A ocurrido un error en el código");	//Por lo que se activa el error
+}
