31546508ac017d3350ef24b8d96d933e8b8867f7
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-20 03:23:57 -0500

    commit ilimicacion de espacio

--- js/36-fetch.js
+++ js/36-fetch.js
@@ -1,24 +1,54 @@
 'use strict'; 
 
 var div_usuarios = document.querySelector("#usuarios");
-
+var div_profesor = document.querySelector("#profesor");
 var div_janet = document.querySelector("#janet");
 //Fetch (ajax) y peticiones a servicios / apis rest
 
-fetch('https://reqres.in/api/users')	//accedemos a los datos remotos (servicio remoto).
-.then(data => data.json())	//Recibimos los datos y los convertimos en json
-.then(users =>{	//Ya que tenemos la información la capturamos en users.
-		usuarios = users.data;	//Guardamos los datos en usuarios.
-		console.log(usuarios);	//Mostramos en consola los datos
+getUsuarios()
+	.then(data => data.json())	//Recibimos los datos y los convertimos en json
+	.then(users =>{	//Ya que tenemos la información la capturamos en users.
+	listadoUsuarios(users.data)
 
+	return getInfo();
+})
+	.then(data =>{
+		div_profesor.innerHTML = data;
+
+		return getJanet();
+	});
+	.then(data => data.json())
+	.then(user =>{
+		mostrarJanet(user.data);
+	})
+	
+
+function getUsuarios(){
 	return fetch('https://reqres.in/api/users')	//accedemos a los datos remotos (servicio remoto).
 }
 	function getJanet(){
+	return fetch('https://reqres.in/api/users/2')	//accedemos a los datos remotos (servicio remoto).
+		}
 
 function getInfo(){
 	var profesor ={
 		nombre: 'Alexo',
 		apellidos: 'Sánchez',
+		url: 'https://www.facebook.com/profile.php?id=100008977205758'
+	};
+	return new Promise((resolve, reject)=>{
+			var profesor_string = "";
+			setTimeout(function(){
+					var profesor_string = JSON.stringify(profesor);
+			if (typeof profesor_string != 'string'|| profesor_string == '') return reject('error');
+
+			return resolve(profesor_string);
+			},3000);
+			
+			
+	});
+
+}
 
 function listadoUsuarios(usuarios){
 		usuarios.map((user, i) =>{
