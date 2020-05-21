c341f0e00d573afca7054a1c3504be260b231362
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-20 21:20:04 -0500

    Adición a video 84
    Agrego minutos y segundos que no se ve en la practica, comentamos los listados para que no aparezcan sin infromación, y corregimos el mes. #14

--- js/38-fechas.js
+++ js/38-fechas.js
@@ -1,15 +1,18 @@
-
+'use strict'
 var fecha = new Date();
 var year = fecha.getFullYear();
 var mes =fecha.getMonth();
 var dia = fecha.getDate();
 var hora = fecha.getHours();
+var minutos = fecha.getMinutes();
+var segundos = fecha.getSeconds();
 
 var textoHora= `
 	El año es: ${year}
-	El mes es: ${mes}
+	El mes es: ${mes +1} //Le sumamos 1 ya que comienza a leer desde 0
 	El dia es: ${dia}
 	La hora es: ${hora}
-
+	Los minutos son: ${minutos}
+	Los segundos son: ${segundos}
 `;
 console.log (textoHora); 
