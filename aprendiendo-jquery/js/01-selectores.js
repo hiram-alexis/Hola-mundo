c0bdccfb4f40fe1635a7b73bd5ffc5a090d61c50
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-22 01:23:59 -0500

    93. Selectores por etiqueta
    Estos son para asignarle atributos por el tipo de etiqueta que tengan. En la practica utilizamos los parrafos como ejemplo. #16

--- aprendiendo-jquery/js/01-selectores.js
+++ aprendiendo-jquery/js/01-selectores.js
@@ -35,6 +35,19 @@ y segundo el valor(red).
 	mi_clase.css("padding","5px");
 
 		$('.sin_borde').click(function(){
+			console.log("Click dado");
 			$(this).addClass('zebra');
+		});
+
+// Selectores de etiqueta
+		var parrafos = $('p').css("cursor","pointer");
+
+		parrafos.click(function(){
+			var that = $(this);
+			if (!that.hasClass('grande')) {
+				that.addClass('grande');
+			}else{
+				that.removeClass('grande');
+			}
 		});
 	});
