5ccb4a9fd672b042488e14debe03288d32dcb56f
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-22 03:25:24 -0500

    97. Hover
    La misma funcion de una forma mas simple. #17

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 	//Eventos MouseOver y MouseOut
-	var caja = $('#caja');
+	 var caja = $('#caja');
+	/*
 	caja.mouseover(function(){
 		$(this).css("background", "red");
 	});
 	caja.mouseout(function(){
 		$(this).css("background", "green");
 	});
+*/
+function cambiaRojo(){
+		$(this).css("background", "red");
+	}
+function cambiaVerde(){
+		$(this).css("background", "green");
+	}
+	//Hover
+	caja.hover(cambiaRojo, cambiaVerde);
 });
