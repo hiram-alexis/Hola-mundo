4ca9cdfc66b0039cbef5cc6b52f98ca1555cb906
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-23 04:20:28 -0500

    102. Efectos en jQuery
    Mas efectos en botones para un elemento. #18

---
+++ aprendiendo-jquery/js/04-efectos.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	//Hide y show
+$("#mostrar").hide();
+
+	$("#mostrar").click(function(){
+		$(this).hide();
+		$("#ocultar").show();
+		//$("#caja").show("fast");	//Recogido Rapido
+		//$("#caja").show("slow");	//Recogido lento
+		//$("#caja").show("normal");	//Recogido Normal
+
+		//$("#caja").fadeIn("slow");	//Desvanecido
+
+		$("#caja").slideDown("slow");	//Desplegar
+
+		//$("#caja").fadeTo("slow", 1);	// Opacidad Minima
+	});
+		$("#ocultar").click(function(){
+			$(this).hide();
+			$("#mostrar").show();
+			//$("#caja").hide("fast");
+			//$("#caja").fadeOut("slow");	//Aparición
+		
+			$("#caja").slideUp("slow");	//Retraer
+
+			//$("#caja").fadeTo("slow", 0);	// Opacidad Maxima
+			});
+
+
+		//En el siguiente codigo hacemos las dos cosas en un solo metodo.
+		$("#todoEnUno").click(function(){
+			//$("#caja").toggle('fast');		//Efecto recoger.
+			//$("#caja").fadeToggle('fast');	//Efecto desbanecer
+			$("#caja").slideToggle('fast');		//Efecto  desplegar
+		});
+
+
+});
