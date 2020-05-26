e152a73f5db20448d12daf040a730a78524ec7e6
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-26 02:28:03 -0500

    103. Animaciones personalizadas
    Poder hacer una serie de animaciones por Jquerry de la manera en que nosotros querramos. #18

--- aprendiendo-jquery/js/04-efectos.js
+++ aprendiendo-jquery/js/04-efectos.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	var caja = $("#caja");
+
 	//Hide y show
 $("#mostrar").hide();
 
@@ -11,7 +13,7 @@ $("#mostrar").hide();
 
 		//$("#caja").fadeIn("slow");	//Desvanecido
 
-		$("#caja").slideDown("slow");	//Desplegar
+		caja.slideDown("slow");	//Desplegar
 
 		//$("#caja").fadeTo("slow", 1);	// Opacidad Minima
 	});
@@ -21,7 +23,7 @@ $("#mostrar").hide();
 			//$("#caja").hide("fast");
 			//$("#caja").fadeOut("slow");	//Aparición
 		
-			$("#caja").slideUp("slow");	//Retraer
+			caja.slideUp("slow");	//Retraer
 
 			//$("#caja").fadeTo("slow", 0);	// Opacidad Maxima
 			});
@@ -31,8 +33,35 @@ $("#mostrar").hide();
 		$("#todoEnUno").click(function(){
 			//$("#caja").toggle('fast');		//Efecto recoger.
 			//$("#caja").fadeToggle('fast');	//Efecto desbanecer
-			$("#caja").slideToggle('fast');		//Efecto  desplegar
+			caja.slideToggle('fast');		//Efecto  desplegar
 		});
 
+		$("#animar").click(function(){
+			
+			caja.animate({
+							marginLeft: '500px',
+							fontSize: '70px',
+							height: '110px'
+					}, 'slow')
+				
+				.animate({
+							borderRadius: '900px',
+							marginTop: '80px'
+					}, 'slow')
+				.animate({
+							borderRadius: '0px',
+							marginLeft: '0px'
+					}, 'slow')
+				.animate({
+							borderRadius: '100px',
+							marginTop: '0px'
+					}, 'slow')
+				.animate({
+							marginLeft: '500px',
+							fontSize: '70px',
+							height: '110px'
+					}, 'slow')
+			;
+		});
 
 });
