2a41d89439a1fbeedc545a59b6939c9224dc2e89
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-26 03:13:42 -0500

    105. Load - Ajax
    Cargar el contenido de alguna peticion Ajax en el objeto creado tipo div. #19

---
+++ aprendiendo-jquery/js/05-ajax.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+/* Metodo Load
+Permite hacer una peticion Ajax por get
+e incrustar la información en un div o 
+de cualquier elemento que se tenga en 
+la pagina.
+*/
+$("#datos").load("https://reqres.in/");
+
+});
