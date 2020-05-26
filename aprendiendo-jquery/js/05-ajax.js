6f2f7a0f0bf6c15f264d73458bb541439f5767f0
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-05-26 03:41:55 -0500

    106. Método Get - AJAX
    Hay un cambio en el codigo original del video ya que la paguina 3 de ese momento ya no es la misma que la actal, por lo que modifique la pagina para que tomara los valores correctos. #19

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -6,6 +6,13 @@ e incrustar la información en un div o
 de cualquier elemento que se tenga en 
 la pagina.
 */
-$("#datos").load("https://reqres.in/");
+//$("#datos").load("https://reqres.in/");
+
+// Metodos Get y¨Post
+	$.get("https://reqres.in/api/users", {page: 2}, function(response){
+		response.data.forEach((element, index) => {
+			$("#datos").append("<p>"+ element.first_name + " "+ element.last_name +"</p>");
+			});
+		});
 
 });
